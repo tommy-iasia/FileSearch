e5e5273aedeb0db925315cceb3104637ebea44ac
lty304 <lty304@ha.org.hk>
2025-08-20 09:40:14 +0800

    first commit

---
+++ FileSearch/Display.cs
@@ -0,0 +1,62 @@
+namespace FileSearch
+{
+    public class Display
+    {
+        public void Show(string line)
+        {
+            Clear();
+
+            try
+            {
+                var width = Console.WindowWidth - 1;
+                if (line.Length > width)
+                {
+                    var head = width / 3;
+                    var tail = width - head - 3;
+
+                    showing = line[..head] + "..." + line[^tail..];
+                }
+                else
+                {
+                    showing = line;
+                }
+            }
+            catch
+            {
+                showing = line;
+            }
+
+            Console.Write(showing);
+        }
+
+        private string showing = string.Empty;
+        public void Clear() => Console.Write($"\r{new string(' ', showing.Length)}\r");
+
+        public void Output(string file)
+        {
+            Clear();
+
+            var color = Console.ForegroundColor;
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            var directory = Path.GetDirectoryName(file);
+            if (!string.IsNullOrEmpty(directory))
+            {
+                Console.Write($"\u001B]8;;{directory}\a{directory}\u001B]8;;\a");
+                Console.Write(Path.DirectorySeparatorChar);
+
+                var name = Path.GetFileName(file);
+                Console.WriteLine($"\u001B]8;;{file}\a{name}\u001B]8;;\a");
+            }
+            else
+            {
+                Console.WriteLine($"\u001B]8;;{file}\a{file}\u001B]8;;\a");
+            }
+
+            Console.ForegroundColor = color;
+
+            Console.Write(showing);
+        }
+    }
+}
---
+++ FileSearch/Program.cs
@@ -0,0 +1,56 @@
+﻿using FileSearch;
+
+var logger = new Logger();
+var capture = await ICapture.LoadAsync(logger);
+var skip = await Skip.LoadAsync(logger);
+var display = new Display();
+var searchedFiles = 0;
+var searchedFolders = 0;
+
+var currentFolders = new List<string> { Environment.CurrentDirectory };
+while (currentFolders.Count > 0)
+{
+    var nextFolders = new List<string>();
+
+    foreach (var currentFolder in currentFolders)
+    {
+        display.Show(currentFolder);
+
+        var files = Directory
+            .GetFiles(currentFolder)
+            .Where(file => !skip.Skipped(file))
+            .OrderByDescending(t => t)
+            .ToArray();
+
+        foreach (var file in files)
+        {
+            display.Show(file);
+
+            var fileText = await File.ReadAllTextAsync(file);
+            if (capture.Capture(fileText))
+            {
+                display.Output(file);
+                await logger.LogAsync(file);
+            }
+        }
+
+        searchedFiles += files.Length;
+
+        var folders = Directory
+            .GetDirectories(currentFolder)
+            .Where(folder => !skip.Skipped(folder))
+            .OrderByDescending(t => t);
+
+        nextFolders.AddRange(folders);
+    }
+
+    searchedFolders += currentFolders.Count;
+
+    currentFolders = nextFolders;
+}
+
+display.Clear();
+
+var end = $"Searched {searchedFiles} files and {searchedFolders} folders.";
+Console.WriteLine(end);
+await logger.LogAsync(end);
